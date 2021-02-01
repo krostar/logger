8c35c9eed66d688b93b165ad9898dbbadcc67bd0
Alexis Destrez <alexis.destrez@pm.me>
2021-02-01 22:49:36 +0100

    Add WriteCloserLevel function

--- writer_level.go
+++ writer_level.go
@@ -6,7 +6,8 @@ import (
 )
 
 type wrappedWriter struct {
-	logFunc func(...interface{})
+	logFunc   func(...interface{})
+	closeFunc func() error
 }
 
 func (ww *wrappedWriter) Write(p []byte) (int, error) {
@@ -15,8 +16,21 @@ func (ww *wrappedWriter) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
+func (ww *wrappedWriter) Close() error {
+	if ww.closeFunc != nil {
+		return ww.closeFunc()
+	}
+	return nil
+}
+
 // WriterLevel returns a writer that writes as if
 // LogAtLevelFunc(l) were called.
 func WriterLevel(l Logger, at Level) io.Writer {
 	return &wrappedWriter{logFunc: LogAtLevelFunc(l, at)}
+}
+
+// WriteCloserLevel returns a writer that writes as if
+// LogAtLevelFunc(l) were called, with a closer.
+func WriteCloserLevel(l Logger, close func() error, at Level) io.WriteCloser {
+	return &wrappedWriter{logFunc: LogAtLevelFunc(l, at), closeFunc: close}
 }
